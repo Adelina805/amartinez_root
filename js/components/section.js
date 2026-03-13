00a5b25148091bd484476edf9bc29cc778642c47
Adelina <152604730+Adelina805@users.noreply.github.com>
2026-03-12 21:02:31 -0700

    feat: add a star to section

--- js/components/section.js
+++ js/components/section.js
@@ -9,6 +9,9 @@ class TimelineSection extends HTMLElement {
 
     this.innerHTML = `
       <section id="${id}" class="timeline-section timeline-section--${theme}">
+        <svg class="hero-star hero-star--1 timeline-star" viewBox="0 0 165 172" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path d="M164.377 87.3351L110.027 112.381L99.847 171.353L59.2313 127.402L9.32766e-07 135.944L29.2488 83.7348L2.8216 30.0421L61.5141 41.7258L104.412 -3.59839e-07L111.438 59.4303L164.377 87.3351Z" fill="currentColor" />
+        </svg>
         <div class="container">
           <div class="section-title">
             <h2 class="section-label">${title}</h2>
