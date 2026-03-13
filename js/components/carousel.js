f3648de53834d0ad2187bc9672fe4b3afb8320b7
Adelina <152604730+Adelina805@users.noreply.github.com>
2026-03-12 21:44:29 -0700

    edit: try to make carosel lighter

--- js/components/carousel.js
+++ js/components/carousel.js
@@ -1,36 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const swipers = [];
-
   document.querySelectorAll(".gallery.swiper").forEach((el) => {
-    const s = new Swiper(el, {
-      loop: true,
+    new Swiper(el, {
+      rewind: true,
+      loop: false,
       speed: 700,
       slidesPerView: 1,
       grabCursor: true,
-      observer: true,
-      observeParents: true,
-      observeSlideChildren: true,
 
       autoplay: {
-        delay: 3000,                
-        disableOnInteraction: false, 
-        pauseOnMouseEnter: true  
+        delay: 3000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true
       },
 
       navigation: {
         nextEl: el.querySelector(".swiper-button-next"),
         prevEl: el.querySelector(".swiper-button-prev")
       }
-    });
-
-    swipers.push(s);
-  });
-
-  let rAF;
-  window.addEventListener("resize", () => {
-    cancelAnimationFrame(rAF);
-    rAF = requestAnimationFrame(() => {
-      swipers.forEach((s) => s.update());
     });
   });
 });
